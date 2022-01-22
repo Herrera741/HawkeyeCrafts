34f79a179cf9b1edf6c8128785f8ba2753430377
Sergio <sherrera725@gmail.com>
2022-01-22 15:33:15 -0800

    style login screen
    - add gradient background color to login screen and asset colors
    
    - clip logo into circular shape on login screen

--- hawkeye-crafts/View/LoginView.swift
+++ hawkeye-crafts/View/LoginView.swift
@@ -11,34 +11,70 @@ struct LoginView: View {
     
     @State private var email = ""
     @State private var password = ""
+    @State private var isSecured = true
     
     var body: some View {
         
-        ZStack {
-
-            Color("hc-pink")
+        ZStack(alignment: .center) {
+            LinearGradient(gradient: Gradient(colors: [Color("hc-pink"), .white, Color("hc-blue")]),
+                startPoint: .top,
+                endPoint: .bottom)
                 .ignoresSafeArea()
             
-            VStack(alignment: .center) {
-                
-                Image("hc-print-logo")
+            VStack {
+                Image("hc-print-logo2")
                     .resizable()
-                    .frame(width: 300, height: 250)
-                
-                TextField("Email", text: self.$email)
-                    .frame(width: 315)
-                    .padding(15)
-                    .background(Color.white)
-                    .cornerRadius(15.0)
-                    .shadow(radius: 8)
-
-                SecureField("Password", text: self.$password)
-                    .frame(width: 315)
-                    .padding(15)
+                    .frame(width: 300, height: 300, alignment: .center)
+                    .clipShape(Circle())
+                    .shadow(radius: 10)
+                    .overlay(Circle().stroke(Color("hc-brown2"), lineWidth: 5))
+                    .padding()
+                    
+                HStack {
+                    TextField("Email", text: self.$email)
+                        .frame(width: 315)
+                        .padding(15)
                         .background(Color.white)
                         .cornerRadius(15.0)
                         .shadow(radius: 8)
-                
+                } // end email HStack
+                .overlay(alignment: .trailing) {
+                    Image(systemName: "envelope.fill")
+                        .foregroundColor(Color.gray)
+                        .scaleEffect(1.2)
+                        .padding(.trailing, 17)
+                } // end overlay
+
+
+                HStack {
+                    if isSecured {
+                        SecureField("Password", text: self.$password)
+                            .frame(width: 315)
+                            .padding(15)
+                                .background(Color.white)
+                                .cornerRadius(15.0)
+                            .shadow(radius: 8)
+                    } else {
+                        TextField("Password", text: self.$password)
+                            .frame(width: 315)
+                            .padding(15)
+                                .background(Color.white)
+                                .cornerRadius(15.0)
+                            .shadow(radius: 8)
+                    }
+                    
+                } // end password HStack
+                .overlay(alignment: .trailing) {
+                    Button(action: {
+                        isSecured.toggle()
+                    }, label: {
+                        Image(systemName: self.isSecured ? "eye.slash" : "eye")
+                            .accentColor(.gray)
+                            .scaleEffect(1.2)
+                            .padding(.trailing, 15)
+                    })
+                } // end overlay
+                    
                 Button(action: {}, label: {
                     Text("Login")
                         .font(.title2)
@@ -60,12 +96,12 @@ struct LoginView: View {
                             .font(.title2)
                             .foregroundColor(Color.blue)
                     })
-                }
-            }
+                } // end signup HStack
+            } // end main VStack
+            .offset(y: -30)
         
-        }
-        
-    }
+        } // end main ZStack
+    } // end body
 }
 
 struct LoginView_Previews: PreviewProvider {
