8d603801978ae87c70c47f20c23d1e8bbc885d86
Sergio <sherrera725@gmail.com>
2022-02-11 04:32:52 -0900

    add plant stool/stand model for home screen data

---
+++ hawkeye-crafts/Data/PlantStoolsData.swift
@@ -0,0 +1,21 @@
+//
+//  PlantStoolsData.swift
+//  hawkeye-crafts
+//
+//  Created by Sergio Herrera on 2/11/22.
+//
+
+import Foundation
+
+let plantStoolsData: [PlantStool] = [
+
+    PlantStool(title: "Stand-1", image: "plant-stand-1"),
+    PlantStool(title: "Stand-2", image: "plant-stand-2"),
+    PlantStool(title: "Stand-3", image: "plant-stand-3"),
+    PlantStool(title: "Stand-4", image: "plant-stand-4"),
+    PlantStool(title: "Stool-1", image: "plant-stool-1"),
+    PlantStool(title: "Stool-2", image: "plant-stool-2"),
+    PlantStool(title: "Stool-3", image: "plant-stool-3"),
+    PlantStool(title: "Stool-4", image: "plant-stool-4"),
+    PlantStool(title: "Stool-5", image: "plant-stool-5")
+]
---
+++ hawkeye-crafts/Models/PlantStoolModel.swift
@@ -0,0 +1,15 @@
+//
+//  PlantStoolModel.swift
+//  hawkeye-crafts
+//
+//  Created by Sergio Herrera on 2/11/22.
+//
+
+import Foundation
+
+struct PlantStool: Identifiable {
+    
+    var id = UUID()
+    var title: String
+    var image: String
+}
