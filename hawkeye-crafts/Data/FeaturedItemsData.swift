9e0954b9aae9e3384043f893bcb55b13dbbde922
Sergio <sherrera725@gmail.com>
2022-02-14 23:56:51 -0800

    add featured items data for featured items view on home screen

---
+++ hawkeye-crafts/Data/FeaturedItemsData.swift
@@ -0,0 +1,16 @@
+//
+//  FeaturedItemsData.swift
+//  hawkeye-crafts
+//
+//  Created by Sergio Herrera on 2/14/22.
+//
+
+import Foundation
+
+let featuredItemsData: [PlantStool] = [
+
+    PlantStool(title: "feat-item-1", image: "feat-item-1"),
+    PlantStool(title: "feat-item-2", image: "feat-item-2"),
+    PlantStool(title: "feat-item-3", image: "feat-item-3")
+]
+
